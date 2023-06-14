beb44e06723a50d6de9a72a73fc4e967ffc8a505
Dhanyal Tag <134966284+TagDhanyal@users.noreply.github.com>
2023-06-14 21:01:06 +0200

    refrence subnet 2

--- main.tf
+++ main.tf
@@ -95,7 +95,7 @@ resource "aws_eks_cluster" "eks_cluster" {
   version  = "1.27"
 
   vpc_config {
-    subnet_ids         = [aws_subnet.eks_subnet.id, aws_subnet.eks_subnet.id]
+    subnet_ids         = [aws_subnet.eks_subnet.id, aws_subnet.eks_subnet2.id]
     security_group_ids = [aws_security_group.eks_sg.id]
   }
 
